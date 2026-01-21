c2210420215e13ee3cc78f16bcc76e157faf34fe
SYAKRIAH <syakrisajalah@gmail.com>
2026-01-21 08:28:52 +0800

    Add mosque information configuration

---
+++ config.ts
@@ -0,0 +1,16 @@
+export const MOSQUE_INFO = {
+    name: "Masjid Raya Al-Mustaqbal",
+    slogan: "Pusat Peradaban & Pemberdayaan Umat",
+    description: "Masjid Al-Mustaqbal hadir sebagai pusat kegiatan ibadah yang nyaman, modern, dan inklusif. Kami berkomitmen untuk membangun masyarakat madani melalui program pendidikan, sosial, dan ekonomi berbasis syariah.",
+    address: "Jl. H. Juanda No. 99, Ciputat, Tangerang Selatan",
+    contact: {
+        phone: "+62 812-3456-7890",
+        email: "info@almustaqbal.id",
+        whatsapp: "6281234567890"
+    },
+    social: {
+        facebook: "Masjid Al Mustaqbal",
+        instagram: "@almustaqbal.official",
+        youtube: "Al-Mustaqbal TV"
+    }
+};
