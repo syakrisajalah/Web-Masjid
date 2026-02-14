3ec8341d118b25360354d7b8a018578b3b048929
SYAKRIAH <syakrisajalah@gmail.com>
2026-02-14 11:43:22 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
   // Memuat variabel lingkungan. Parameter ketiga '' memuat semua variabel tanpa filter prefix.
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
 
   return {
     plugins: [react()],
